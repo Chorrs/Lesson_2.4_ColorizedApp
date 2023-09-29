a0ecb65547bd022ca2cd87bd394e97891a5553ec
Ekaterina Pecherskaya <Pechorskaya.K8@gmail.com>
2023-09-29 22:12:54 +0300

    Initial Commit

---
+++ Lesson_2.4_ColorizedApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson_2.4_ColorizedApp
+//
+//  Created by Chorrs on 29.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
